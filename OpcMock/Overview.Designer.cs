66507c86d82a5c7b2afca0326f8e34d80aa3aa2b
msdeibel <git2014@markus-deibel.de>
2015-02-25 20:02:22 +0100

    Initial commit of project files

---
+++ OpcMock/Overview.Designer.cs
@@ -0,0 +1,180 @@
+﻿using System;
+namespace WindowsFormsApplication1
+{
+    partial class Overview
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvOpcData = new System.Windows.Forms.DataGridView();
+            this.TagName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TagValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TagQualityText = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.TagQualityValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tbProjectFileName = new System.Windows.Forms.TextBox();
+            this.lblProjectFileName = new System.Windows.Forms.Label();
+            this.btnSaveData = new System.Windows.Forms.Button();
+            this.btnReadOpcData = new System.Windows.Forms.Button();
+            this.projectFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.btnProjectFileDialog = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOpcData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvOpcData
+            // 
+            this.dgvOpcData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOpcData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TagName,
+            this.TagValue,
+            this.TagQualityText,
+            this.TagQualityValue});
+            this.dgvOpcData.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
+            this.dgvOpcData.Location = new System.Drawing.Point(2, 53);
+            this.dgvOpcData.Name = "dgvOpcData";
+            this.dgvOpcData.Size = new System.Drawing.Size(789, 202);
+            this.dgvOpcData.TabIndex = 0;
+            // 
+            // TagName
+            // 
+            this.TagName.HeaderText = "TagName";
+            this.TagName.Name = "TagName";
+            // 
+            // TagValue
+            // 
+            this.TagValue.HeaderText = "TagValue";
+            this.TagValue.Name = "TagValue";
+            // 
+            // TagQualityText
+            // 
+            dataGridViewCellStyle1.NullValue = "Good";
+            this.TagQualityText.DefaultCellStyle = dataGridViewCellStyle1;
+            this.TagQualityText.HeaderText = "TagQualityText";
+            this.TagQualityText.Name = "TagQualityText";
+            this.TagQualityText.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.TagQualityText.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // TagQualityValue
+            // 
+            dataGridViewCellStyle2.NullValue = "192";
+            this.TagQualityValue.DefaultCellStyle = dataGridViewCellStyle2;
+            this.TagQualityValue.HeaderText = "TagQualityValue";
+            this.TagQualityValue.Name = "TagQualityValue";
+            this.TagQualityValue.ReadOnly = true;
+            // 
+            // tbProjectFileName
+            // 
+            this.tbProjectFileName.Location = new System.Drawing.Point(2, 27);
+            this.tbProjectFileName.Name = "tbProjectFileName";
+            this.tbProjectFileName.Size = new System.Drawing.Size(528, 20);
+            this.tbProjectFileName.TabIndex = 1;
+            this.tbProjectFileName.Text = "d:\\temp\\opctest";
+            // 
+            // lblProjectFileName
+            // 
+            this.lblProjectFileName.AutoSize = true;
+            this.lblProjectFileName.Location = new System.Drawing.Point(-1, 11);
+            this.lblProjectFileName.Name = "lblProjectFileName";
+            this.lblProjectFileName.Size = new System.Drawing.Size(85, 13);
+            this.lblProjectFileName.TabIndex = 2;
+            this.lblProjectFileName.Text = "Project file name";
+            // 
+            // btnSaveData
+            // 
+            this.btnSaveData.Location = new System.Drawing.Point(2, 261);
+            this.btnSaveData.Name = "btnSaveData";
+            this.btnSaveData.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveData.TabIndex = 4;
+            this.btnSaveData.Text = "Save data";
+            this.btnSaveData.UseVisualStyleBackColor = true;
+            this.btnSaveData.Click += new System.EventHandler(this.btnSaveData_Click);
+            // 
+            // btnReadOpcData
+            // 
+            this.btnReadOpcData.Location = new System.Drawing.Point(360, 261);
+            this.btnReadOpcData.Name = "btnReadOpcData";
+            this.btnReadOpcData.Size = new System.Drawing.Size(75, 23);
+            this.btnReadOpcData.TabIndex = 5;
+            this.btnReadOpcData.Text = "Read data";
+            this.btnReadOpcData.UseVisualStyleBackColor = true;
+            this.btnReadOpcData.Click += new System.EventHandler(this.btnReadOpcData_Click);
+            // 
+            // projectFileDialog
+            // 
+            this.projectFileDialog.Filter = "OPC Mock Daten|*.omd";
+            // 
+            // btnProjectFileDialog
+            // 
+            this.btnProjectFileDialog.AutoSize = true;
+            this.btnProjectFileDialog.Location = new System.Drawing.Point(536, 25);
+            this.btnProjectFileDialog.Name = "btnProjectFileDialog";
+            this.btnProjectFileDialog.Size = new System.Drawing.Size(26, 23);
+            this.btnProjectFileDialog.TabIndex = 6;
+            this.btnProjectFileDialog.Text = "...";
+            this.btnProjectFileDialog.UseVisualStyleBackColor = true;
+            this.btnProjectFileDialog.Click += new System.EventHandler(this.btnProjectFileDialog_Click);
+            // 
+            // Overview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 445);
+            this.Controls.Add(this.btnProjectFileDialog);
+            this.Controls.Add(this.btnReadOpcData);
+            this.Controls.Add(this.btnSaveData);
+            this.Controls.Add(this.lblProjectFileName);
+            this.Controls.Add(this.tbProjectFileName);
+            this.Controls.Add(this.dgvOpcData);
+            this.Name = "Overview";
+            this.Text = "OPC Mock";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOpcData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        
+
+        
+
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvOpcData;
+        private System.Windows.Forms.TextBox tbProjectFileName;
+        private System.Windows.Forms.Label lblProjectFileName;
+        private System.Windows.Forms.Button btnSaveData;
+        private System.Windows.Forms.Button btnReadOpcData;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TagName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TagValue;
+        private System.Windows.Forms.DataGridViewComboBoxColumn TagQualityText;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TagQualityValue;
+        private System.Windows.Forms.SaveFileDialog projectFileDialog;
+        private System.Windows.Forms.Button btnProjectFileDialog;
+    }
+}
+
---
+++ OpcMock/Overview.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+namespace WindowsFormsApplication1
+{
+    public partial class Overview : Form
+    {
+        public enum OpcTagQuality
+        { 
+            Bad = 0,
+            Unknown = 8,
+            Good = 192
+        }
+
+        private int LOCK_ACQUISITION_RETRY_INTERVALL_IN_MS = 500;
+        private string FILE_EXT_DATA = ".omd";
+        private string FILE_EXT_LOCK = ".lck";
+
+        private string projectFilePath;
+        private string lockFilePath;
+
+        public Overview()
+        {
+            InitializeComponent();
+
+            projectFilePath = string.Empty;
+            lockFilePath = string.Empty;
+
+            SetDgvPropertiesThatTheDesignerKeepsLosing();
+        }
+
+        private void SetDgvPropertiesThatTheDesignerKeepsLosing()
+        {
+            this.TagQualityText.DataSource = Enum.GetNames(typeof(OpcTagQuality));
+            this.dgvOpcData.CurrentCellDirtyStateChanged += dgvOpcData_CurrentCellDirtyStateChanged;
+        }
+
+        private void btnReadOpcData_Click(object sender, EventArgs e)
+        {
+            ReadOpcFile();
+        }
+
+        private void ReadOpcFile()
+        {
+            try
+            {
+                CheckProjectFilePathValue();
+
+                WaitForAndAcquireFileLock();
+
+                string[] opcLines = GetOpcData(projectFilePath);
+
+                FillOpcDataGrid(opcLines);
+            }
+            catch (NoProjectFileException)
+            {
+                MessageBox.Show("Error!\nProject file must be set.");
+            }
+            catch (ProjectFileErrorException)
+            {
+                MessageBox.Show("Error!\nCheck existence and permissions for project file " + projectFilePath);
+            }
+            finally
+            {
+                ReleaseFileLock();
+            }
+        }
+
+        private void FillOpcDataGrid(string[] opcLines)
+        {
+            dgvOpcData.Rows.Clear();
+
+            foreach (string s in opcLines)
+            {
+                string[] opcTagParts = s.Split(';');
+
+                int newRowIndex = dgvOpcData.Rows.Add();
+
+                dgvOpcData.Rows[newRowIndex].Cells[0].Value = opcTagParts[0];
+                dgvOpcData.Rows[newRowIndex].Cells[1].Value = opcTagParts[1];
+                dgvOpcData.Rows[newRowIndex].Cells[2].Value = opcTagParts[2];
+                dgvOpcData.Rows[newRowIndex].Cells[3].Value = opcTagParts[3];
+            }
+        }
+
+        void dgvOpcData_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            DataGridViewCell currentCell = dgvOpcData.CurrentCell;
+
+            dgvOpcData.CommitEdit(new DataGridViewDataErrorContexts());
+
+            if (currentCell.OwningColumn.Name.Equals("TagQualityText"))
+            {
+                string workingValue = (currentCell.Value != null) ? currentCell.Value.ToString() : "192";
+
+                dgvOpcData.Rows[currentCell.RowIndex].Cells["TagQualityValue"].Value = ((int)(Enum.Parse(typeof(OpcTagQuality), workingValue))).ToString();
+            }
+        }
+
+        private void btnSaveData_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(projectFilePath))
+            {
+                MessageBox.Show("Set target file name!");
+                
+                return;
+            }
+
+            WaitForAndAcquireFileLock();
+
+            WriteDataToProjectFile();
+        }
+
+        private void WaitForAndAcquireFileLock()
+        {
+            bool lockAcquired = false;
+
+            while (!lockAcquired)
+            {
+                try
+                {
+                    File.Create(lockFilePath).Close();
+
+                    lockAcquired = true;
+                }
+                catch (Exception)
+                {
+                    Thread.Sleep(LOCK_ACQUISITION_RETRY_INTERVALL_IN_MS);
+                }
+            }
+        }
+
+        private void WriteDataToProjectFile()
+        {
+            FileStream dataFileStream = File.Open(projectFilePath, FileMode.Create);
+
+            StreamWriter streamWriter = new StreamWriter(dataFileStream);
+
+            try
+            {
+                if (dgvOpcData.RowCount > 0)
+                {
+                    foreach (DataGridViewRow dgvr in dgvOpcData.Rows)
+                    {
+
+                        if (dgvr.Index < dgvOpcData.RowCount - 1)
+                        {
+                            streamWriter.WriteLine(dgvr.Cells[0].FormattedValue.ToString() + ';'
+                                                    + dgvr.Cells[1].FormattedValue.ToString() + ';'
+                                                    + dgvr.Cells[2].FormattedValue.ToString() + ';'
+                                                    + dgvr.Cells[3].FormattedValue.ToString()
+                                                    );
+                        }
+                    }
+                }
+
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+            catch (Exception ex)
+            {
+                //void
+            }
+            finally
+            {
+                dataFileStream.Close();
+                ReleaseFileLock();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetFilePath"></param>
+        /// <exception cref="MockOPC.NotProjectFileException"></exception>
+        private void CheckProjectFilePathValue()
+        {
+            if (string.IsNullOrWhiteSpace(projectFilePath))
+            {
+                throw new NoProjectFileException();
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="targetFilePath"></param>
+        /// <exception cref="MockOPC.ProjectFileErrorException"></exception>
+        private string[] GetOpcData(string targetFilePath)
+        {
+            try
+            {
+                return File.ReadAllLines(targetFilePath);
+            }
+            catch (Exception)
+            {
+                throw new ProjectFileErrorException();
+            }
+        }
+
+        private void ReleaseFileLock()
+        {
+            File.Delete(lockFilePath);
+        }
+
+        private void btnProjectFileDialog_Click(object sender, EventArgs e)
+        {
+            if(DialogResult.OK.Equals(projectFileDialog.ShowDialog(this)))
+            {
+                projectFilePath = projectFileDialog.FileName;
+                File.Create(projectFilePath).Close();
+
+                lockFilePath = projectFilePath.Replace(FILE_EXT_DATA, FILE_EXT_LOCK);
+            }
+        }
+    }
+}
