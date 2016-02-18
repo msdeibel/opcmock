b6f0f3ebd2533a1fed01bb50dbeca7c93e6fa26f
msdeibel <git2014@markus-deibel.de>
2016-02-18 21:18:00 +0100

    Set project file based on name and path
    The separation of project name, folder and filename was not clear until
    now. The separation for this is improved.

--- OpcMock/DemoForm.Designer.cs
+++ OpcMock/DemoForm.Designer.cs
@@ -29,8 +29,8 @@ namespace OpcMock
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dgvOpcData = new System.Windows.Forms.DataGridView();
             this.TagName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TagValue = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -41,16 +41,21 @@ namespace OpcMock
             this.btnSaveData = new System.Windows.Forms.Button();
             this.btnReadOpcData = new System.Windows.Forms.Button();
             this.sfdDataFile = new System.Windows.Forms.SaveFileDialog();
-            this.btnDataFileDialog = new System.Windows.Forms.Button();
             this.lblProtocol = new System.Windows.Forms.Label();
             this.btnStep = new System.Windows.Forms.Button();
             this.rtbProtocol = new System.Windows.Forms.RichTextBox();
-            this.lblProjectFilePath = new System.Windows.Forms.Label();
+            this.lblProjectPath = new System.Windows.Forms.Label();
             this.tbProjectFilePath = new System.Windows.Forms.TextBox();
             this.sfdProjectFile = new System.Windows.Forms.SaveFileDialog();
-            this.btnProjectFileDialog = new System.Windows.Forms.Button();
             this.btnSaveProjectFile = new System.Windows.Forms.Button();
+            this.lblProjectName = new System.Windows.Forms.Label();
+            this.tbProjectName = new System.Windows.Forms.TextBox();
+            this.fbdProjectPath = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnFdbDialog = new System.Windows.Forms.Button();
+            this.gbProjectInformation = new System.Windows.Forms.GroupBox();
+            this.btnCreateProject = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvOpcData)).BeginInit();
+            this.gbProjectInformation.SuspendLayout();
             this.SuspendLayout();
             // 
             // dgvOpcData
@@ -62,9 +67,9 @@ namespace OpcMock
             this.TagQualityText,
             this.TagQualityValue});
             this.dgvOpcData.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnEnter;
-            this.dgvOpcData.Location = new System.Drawing.Point(2, 110);
+            this.dgvOpcData.Location = new System.Drawing.Point(12, 209);
             this.dgvOpcData.Name = "dgvOpcData";
-            this.dgvOpcData.Size = new System.Drawing.Size(560, 202);
+            this.dgvOpcData.Size = new System.Drawing.Size(560, 210);
             this.dgvOpcData.TabIndex = 0;
             // 
             // TagName
@@ -79,8 +84,8 @@ namespace OpcMock
             // 
             // TagQualityText
             // 
-            dataGridViewCellStyle1.NullValue = "Good";
-            this.TagQualityText.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.NullValue = "Good";
+            this.TagQualityText.DefaultCellStyle = dataGridViewCellStyle3;
             this.TagQualityText.HeaderText = "TagQualityText";
             this.TagQualityText.Name = "TagQualityText";
             this.TagQualityText.Resizable = System.Windows.Forms.DataGridViewTriState.True;
@@ -88,15 +93,15 @@ namespace OpcMock
             // 
             // TagQualityValue
             // 
-            dataGridViewCellStyle2.NullValue = "192";
-            this.TagQualityValue.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.NullValue = "192";
+            this.TagQualityValue.DefaultCellStyle = dataGridViewCellStyle4;
             this.TagQualityValue.HeaderText = "TagQualityValue";
             this.TagQualityValue.Name = "TagQualityValue";
             this.TagQualityValue.ReadOnly = true;
             // 
             // tbDataFileName
             // 
-            this.tbDataFileName.Location = new System.Drawing.Point(2, 84);
+            this.tbDataFileName.Location = new System.Drawing.Point(10, 128);
             this.tbDataFileName.Name = "tbDataFileName";
             this.tbDataFileName.Size = new System.Drawing.Size(528, 20);
             this.tbDataFileName.TabIndex = 1;
@@ -105,7 +110,7 @@ namespace OpcMock
             // lblDataFileName
             // 
             this.lblDataFileName.AutoSize = true;
-            this.lblDataFileName.Location = new System.Drawing.Point(-1, 68);
+            this.lblDataFileName.Location = new System.Drawing.Point(8, 112);
             this.lblDataFileName.Name = "lblDataFileName";
             this.lblDataFileName.Size = new System.Drawing.Size(67, 13);
             this.lblDataFileName.TabIndex = 2;
@@ -114,9 +119,9 @@ namespace OpcMock
             // btnSaveData
             // 
             this.btnSaveData.Enabled = false;
-            this.btnSaveData.Location = new System.Drawing.Point(2, 318);
+            this.btnSaveData.Location = new System.Drawing.Point(12, 429);
             this.btnSaveData.Name = "btnSaveData";
-            this.btnSaveData.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveData.Size = new System.Drawing.Size(75, 31);
             this.btnSaveData.TabIndex = 4;
             this.btnSaveData.Text = "Save data";
             this.btnSaveData.UseVisualStyleBackColor = true;
@@ -125,29 +130,18 @@ namespace OpcMock
             // btnReadOpcData
             // 
             this.btnReadOpcData.Enabled = false;
-            this.btnReadOpcData.Location = new System.Drawing.Point(83, 318);
+            this.btnReadOpcData.Location = new System.Drawing.Point(93, 429);
             this.btnReadOpcData.Name = "btnReadOpcData";
-            this.btnReadOpcData.Size = new System.Drawing.Size(75, 23);
+            this.btnReadOpcData.Size = new System.Drawing.Size(75, 31);
             this.btnReadOpcData.TabIndex = 5;
             this.btnReadOpcData.Text = "Read data";
             this.btnReadOpcData.UseVisualStyleBackColor = true;
             this.btnReadOpcData.Click += new System.EventHandler(this.btnReadOpcData_Click);
             // 
-            // btnDataFileDialog
-            // 
-            this.btnDataFileDialog.AutoSize = true;
-            this.btnDataFileDialog.Location = new System.Drawing.Point(536, 82);
-            this.btnDataFileDialog.Name = "btnDataFileDialog";
-            this.btnDataFileDialog.Size = new System.Drawing.Size(26, 23);
-            this.btnDataFileDialog.TabIndex = 6;
-            this.btnDataFileDialog.Text = "...";
-            this.btnDataFileDialog.UseVisualStyleBackColor = true;
-            this.btnDataFileDialog.Click += new System.EventHandler(this.btnDataFileDialog_Click);
-            // 
             // lblProtocol
             // 
             this.lblProtocol.AutoSize = true;
-            this.lblProtocol.Location = new System.Drawing.Point(590, 91);
+            this.lblProtocol.Location = new System.Drawing.Point(600, 193);
             this.lblProtocol.Name = "lblProtocol";
             this.lblProtocol.Size = new System.Drawing.Size(46, 13);
             this.lblProtocol.TabIndex = 8;
@@ -156,9 +150,9 @@ namespace OpcMock
             // btnStep
             // 
             this.btnStep.Enabled = false;
-            this.btnStep.Location = new System.Drawing.Point(593, 318);
+            this.btnStep.Location = new System.Drawing.Point(603, 429);
             this.btnStep.Name = "btnStep";
-            this.btnStep.Size = new System.Drawing.Size(127, 23);
+            this.btnStep.Size = new System.Drawing.Size(127, 31);
             this.btnStep.TabIndex = 12;
             this.btnStep.Text = "Execute step 1";
             this.btnStep.UseVisualStyleBackColor = true;
@@ -166,71 +160,111 @@ namespace OpcMock
             // 
             // rtbProtocol
             // 
-            this.rtbProtocol.Location = new System.Drawing.Point(593, 110);
+            this.rtbProtocol.Location = new System.Drawing.Point(603, 209);
             this.rtbProtocol.Name = "rtbProtocol";
-            this.rtbProtocol.Size = new System.Drawing.Size(287, 202);
+            this.rtbProtocol.Size = new System.Drawing.Size(287, 210);
             this.rtbProtocol.TabIndex = 13;
             this.rtbProtocol.Text = "Set;tagToSet;newValue;192\nDummy\nWait;tagToWaitFor;expectedValue;192\n";
             // 
-            // lblProjectFilePath
+            // lblProjectPath
             // 
-            this.lblProjectFilePath.AutoSize = true;
-            this.lblProjectFilePath.Location = new System.Drawing.Point(0, 9);
-            this.lblProjectFilePath.Name = "lblProjectFilePath";
-            this.lblProjectFilePath.Size = new System.Drawing.Size(77, 13);
-            this.lblProjectFilePath.TabIndex = 14;
-            this.lblProjectFilePath.Text = "Projectfile path";
+            this.lblProjectPath.AutoSize = true;
+            this.lblProjectPath.Location = new System.Drawing.Point(7, 66);
+            this.lblProjectPath.Name = "lblProjectPath";
+            this.lblProjectPath.Size = new System.Drawing.Size(64, 13);
+            this.lblProjectPath.TabIndex = 14;
+            this.lblProjectPath.Text = "Project path";
             // 
             // tbProjectFilePath
             // 
-            this.tbProjectFilePath.Location = new System.Drawing.Point(3, 25);
+            this.tbProjectFilePath.Location = new System.Drawing.Point(10, 82);
             this.tbProjectFilePath.Name = "tbProjectFilePath";
             this.tbProjectFilePath.Size = new System.Drawing.Size(528, 20);
             this.tbProjectFilePath.TabIndex = 15;
-            this.tbProjectFilePath.Text = "d:\\temp\\opctest";
-            // 
-            // btnProjectFileDialog
-            // 
-            this.btnProjectFileDialog.AutoSize = true;
-            this.btnProjectFileDialog.Location = new System.Drawing.Point(537, 22);
-            this.btnProjectFileDialog.Name = "btnProjectFileDialog";
-            this.btnProjectFileDialog.Size = new System.Drawing.Size(26, 23);
-            this.btnProjectFileDialog.TabIndex = 16;
-            this.btnProjectFileDialog.Text = "...";
-            this.btnProjectFileDialog.UseVisualStyleBackColor = true;
-            this.btnProjectFileDialog.Click += new System.EventHandler(this.btnSaveDataFile_Click);
+            this.tbProjectFilePath.Text = "d:\\temp\\opcmock";
             // 
             // btnSaveProjectFile
             // 
-            this.btnSaveProjectFile.Location = new System.Drawing.Point(593, 22);
+            this.btnSaveProjectFile.Location = new System.Drawing.Point(727, 76);
             this.btnSaveProjectFile.Name = "btnSaveProjectFile";
-            this.btnSaveProjectFile.Size = new System.Drawing.Size(75, 23);
+            this.btnSaveProjectFile.Size = new System.Drawing.Size(151, 31);
             this.btnSaveProjectFile.TabIndex = 17;
             this.btnSaveProjectFile.Text = "Save project";
             this.btnSaveProjectFile.UseVisualStyleBackColor = true;
             this.btnSaveProjectFile.Click += new System.EventHandler(this.btnSaveProjectFile_Click);
             // 
+            // lblProjectName
+            // 
+            this.lblProjectName.AutoSize = true;
+            this.lblProjectName.Location = new System.Drawing.Point(6, 16);
+            this.lblProjectName.Name = "lblProjectName";
+            this.lblProjectName.Size = new System.Drawing.Size(69, 13);
+            this.lblProjectName.TabIndex = 18;
+            this.lblProjectName.Text = "Project name";
+            // 
+            // tbProjectName
+            // 
+            this.tbProjectName.Location = new System.Drawing.Point(10, 32);
+            this.tbProjectName.Name = "tbProjectName";
+            this.tbProjectName.Size = new System.Drawing.Size(194, 20);
+            this.tbProjectName.TabIndex = 19;
+            this.tbProjectName.Text = "testProject001";
+            // 
+            // btnFdbDialog
+            // 
+            this.btnFdbDialog.Location = new System.Drawing.Point(544, 79);
+            this.btnFdbDialog.Name = "btnFdbDialog";
+            this.btnFdbDialog.Size = new System.Drawing.Size(45, 23);
+            this.btnFdbDialog.TabIndex = 20;
+            this.btnFdbDialog.Text = "Path";
+            this.btnFdbDialog.UseVisualStyleBackColor = true;
+            this.btnFdbDialog.Click += new System.EventHandler(this.btnFdbDialog_Click);
+            // 
+            // gbProjectInformation
+            // 
+            this.gbProjectInformation.Controls.Add(this.btnCreateProject);
+            this.gbProjectInformation.Controls.Add(this.btnSaveProjectFile);
+            this.gbProjectInformation.Controls.Add(this.tbProjectName);
+            this.gbProjectInformation.Controls.Add(this.btnFdbDialog);
+            this.gbProjectInformation.Controls.Add(this.lblProjectName);
+            this.gbProjectInformation.Controls.Add(this.tbProjectFilePath);
+            this.gbProjectInformation.Controls.Add(this.tbDataFileName);
+            this.gbProjectInformation.Controls.Add(this.lblDataFileName);
+            this.gbProjectInformation.Controls.Add(this.lblProjectPath);
+            this.gbProjectInformation.Location = new System.Drawing.Point(12, 12);
+            this.gbProjectInformation.Name = "gbProjectInformation";
+            this.gbProjectInformation.Size = new System.Drawing.Size(884, 168);
+            this.gbProjectInformation.TabIndex = 21;
+            this.gbProjectInformation.TabStop = false;
+            this.gbProjectInformation.Text = "Project information";
+            // 
+            // btnCreateProject
+            // 
+            this.btnCreateProject.Location = new System.Drawing.Point(727, 16);
+            this.btnCreateProject.Name = "btnCreateProject";
+            this.btnCreateProject.Size = new System.Drawing.Size(151, 31);
+            this.btnCreateProject.TabIndex = 21;
+            this.btnCreateProject.Text = "Create project";
+            this.btnCreateProject.UseVisualStyleBackColor = true;
+            this.btnCreateProject.Click += new System.EventHandler(this.btnCreateProject_Click);
+            // 
             // DemoForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(892, 445);
-            this.Controls.Add(this.btnSaveProjectFile);
-            this.Controls.Add(this.btnProjectFileDialog);
-            this.Controls.Add(this.tbProjectFilePath);
-            this.Controls.Add(this.lblProjectFilePath);
+            this.ClientSize = new System.Drawing.Size(908, 505);
+            this.Controls.Add(this.gbProjectInformation);
             this.Controls.Add(this.rtbProtocol);
             this.Controls.Add(this.btnStep);
             this.Controls.Add(this.lblProtocol);
-            this.Controls.Add(this.btnDataFileDialog);
             this.Controls.Add(this.btnReadOpcData);
             this.Controls.Add(this.btnSaveData);
-            this.Controls.Add(this.lblDataFileName);
-            this.Controls.Add(this.tbDataFileName);
             this.Controls.Add(this.dgvOpcData);
             this.Name = "DemoForm";
             this.Text = "OPC Mock";
             ((System.ComponentModel.ISupportInitialize)(this.dgvOpcData)).EndInit();
+            this.gbProjectInformation.ResumeLayout(false);
+            this.gbProjectInformation.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -253,15 +287,19 @@ namespace OpcMock
         private System.Windows.Forms.DataGridViewComboBoxColumn TagQualityText;
         private System.Windows.Forms.DataGridViewTextBoxColumn TagQualityValue;
         private System.Windows.Forms.SaveFileDialog sfdDataFile;
-        private System.Windows.Forms.Button btnDataFileDialog;
         private System.Windows.Forms.Label lblProtocol;
         private System.Windows.Forms.Button btnStep;
         private System.Windows.Forms.RichTextBox rtbProtocol;
-        private System.Windows.Forms.Label lblProjectFilePath;
+        private System.Windows.Forms.Label lblProjectPath;
         private System.Windows.Forms.TextBox tbProjectFilePath;
         private System.Windows.Forms.SaveFileDialog sfdProjectFile;
-        private System.Windows.Forms.Button btnProjectFileDialog;
         private System.Windows.Forms.Button btnSaveProjectFile;
+        private System.Windows.Forms.Label lblProjectName;
+        private System.Windows.Forms.TextBox tbProjectName;
+        private System.Windows.Forms.FolderBrowserDialog fbdProjectPath;
+        private System.Windows.Forms.Button btnFdbDialog;
+        private System.Windows.Forms.GroupBox gbProjectInformation;
+        private System.Windows.Forms.Button btnCreateProject;
     }
 }
 
