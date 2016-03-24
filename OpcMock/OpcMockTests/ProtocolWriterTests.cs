c11a79f26791cc5d27d4da4576e384ebc681292b
msdeibel <git2014@markus-deibel.de>
2016-03-24 21:01:03 +0100

    Add ProtocolWriter for saving protocols to disk as CSV (; as separator)

---
+++ OpcMock/OpcMockTests/ProtocolWriterTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpcMock;
+using System;
+using System.IO;
+
+namespace OpcMockTests
+{
+    [TestClass]
+    public class ProtocolWriterTests : OpcMockTestsBase
+    {
+        [TestMethod]
+        public void ProtocolWriter_Is_Created_With_A_Folder_Path()
+        {
+            string folderPath = "folderPath";
+            string projectName = "projectName";
+
+            ProtocolWriter protocolWriter = new ProtocolWriter(folderPath, projectName);
+
+            Assert.AreEqual(folderPath, protocolWriter.FolderPath);
+            Assert.AreEqual(projectName, protocolWriter.ProjectName);
+        }
+
+        [TestMethod]
+        public void Save_Creates_A_Protocol_File_In_The_FolderPath()
+        {
+            string projectName = "projectName";
+            string protocolName = "protocolName";
+
+            OpcMockProtocol opcMockProtocol = new OpcMockProtocol(protocolName);
+
+            ProtocolWriter protocolWriter = new ProtocolWriter(TestContext.TestDeploymentDir, projectName);
+
+            protocolWriter.Save(opcMockProtocol);
+
+            string protocolFilePath = TestContext.TestDeploymentDir + Path.DirectorySeparatorChar + projectName + FileExtensionContants.FileExtensionProtocol;
+
+            Assert.IsTrue(File.Exists(protocolFilePath));
+
+            File.Delete(protocolFilePath);
+        }
+
+        [TestMethod]
+        public void Save_Stores_ProtocolLines_In_The_File()
+        {
+            string projectName = "projectName";
+            string protocolName = "protocolName";
+
+            OpcMockProtocol omp = new OpcMockProtocol(protocolName);
+            string ompLine1 = "Set;tagPath1;tagValue1;192";
+            string ompLine2 = "Set;tagPath2;tagValue2;192";
+            omp.Append(new ProtocolLine(ompLine1));
+            omp.Append(new ProtocolLine(ompLine2));
+
+            string protocolFilePath = TestContext.TestDeploymentDir + Path.DirectorySeparatorChar + projectName + FileExtensionContants.FileExtensionProtocol;
+
+            ProtocolWriter protocolWriter = new ProtocolWriter(TestContext.TestDeploymentDir, projectName);
+
+            protocolWriter.Save(omp);
+
+            string expectedFileContent = ompLine1 + Environment.NewLine + ompLine2 + Environment.NewLine;
+
+            string actualFileContent = File.ReadAllText(protocolFilePath);
+
+            Assert.AreEqual(expectedFileContent, actualFileContent);
+
+            File.Delete(protocolFilePath);
+        }
+    }
+}
---
+++ OpcMock/ProtocolWriter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpcMock
+{
+    public class ProtocolWriter
+    {
+        public string FolderPath { get; internal set; }
+        public string ProjectName { get; internal set; }
+
+        public ProtocolWriter(string folderPath, string projectName)
+        {
+            FolderPath = folderPath;
+            ProjectName = projectName;
+        }
+
+        public void Save(OpcMockProtocol opcMockProtocol)
+        {
+            string protocolFilePath = FolderPath + Path.DirectorySeparatorChar + ProjectName + FileExtensionContants.FileExtensionProtocol;
+
+            File.Create(protocolFilePath).Close();
+
+            StringBuilder fileContentBuilder = new StringBuilder();
+
+            foreach (ProtocolLine pl in opcMockProtocol.Lines)
+            {
+                fileContentBuilder.Append(pl.ToString() + Environment.NewLine);
+            }
+
+            File.WriteAllText(protocolFilePath, fileContentBuilder.ToString());
+        }
+    }
+}
